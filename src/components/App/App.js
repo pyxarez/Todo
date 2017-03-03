5a715e1cd4375a8667c8fadf28214696b784dfc2
Alexander <pyxarez@gmail.com>
2017-03-03 23:44:48 +0400

    Header has been created

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header-container">
+        <h1 className="Header-title">To-Do List</h1>
+        <label className="Header-check-done"><input type="checkbox"/>Show done</label>
+        <div className="Header-filter-constainer">
+          <input className="Header-filter" type="text" placeholder="Search" />
+          <button className="Header-filter-clear"></button>
+        </div>
+      </div>
+    );
+  }
+}
