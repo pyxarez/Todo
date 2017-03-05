5a271db5023ec2171e334292493e8aa489da4ead
Alexander <pyxarez@gmail.com>
2017-03-05 23:18:51 +0400

    Добавил progress bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,12 @@ export default class Header extends Component {
       <div className="Header-container">
         <h1 className="Header-title">To-Do List</h1>
         <label className="Header-check-done"><input type="checkbox"/>Show done</label>
-        <div className="Header-filter-constainer">
+        <div className="Header-filter-container">
           <input className="Header-filter" type="text" placeholder="Search" />
           <button className="Header-filter-clear"></button>
+        </div>
+        <div className="Header-progress-bar">
+          <div className="Header-progress"></div>
         </div>
       </div>
     );
